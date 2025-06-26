e069452431f7da3e0c56e1693df4636cd83f6918
complianceY <Compliance@freshvnf.com>
2025-06-27 02:03:04 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -299,7 +299,7 @@ function App() {
         }}
       >
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex justify-between items-center h-16">
+          <div className="flex justify-between items-center h-20">
             <div className="flex items-center space-x-3 group cursor-pointer">
               <div className="relative">
                 <img 
